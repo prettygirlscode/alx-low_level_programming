7b790a60d5c7f9847781ca95753570e5122e8123
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-13 19:05:23 +0100

    print string without using printf function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/*
+ * A c program that prints 'programming is like building a multilingual puzzle'
+ * without using the 'printf' function
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
