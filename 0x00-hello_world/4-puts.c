d35f7d0d8b6308bc0259da782a2ca6762479aae0
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-13 19:20:58 +0100

    print string without using printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,11 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point
+ * Despription: 'printing a string without using the printf function
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
