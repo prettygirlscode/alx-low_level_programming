04b8cc2d49721fa70cb51897b095f8b2f9ef50b5
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-13 19:45:07 +0100

    printing a program using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: A C program that prints using the function 'printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+
+	return (0);
+}
