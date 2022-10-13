41938a3892d27b9d33826c22e4ce035834b9372d
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-13 22:27:52 +0100

    printing size of various data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float e;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %1d byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
