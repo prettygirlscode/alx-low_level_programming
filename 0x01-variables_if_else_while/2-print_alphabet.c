a73a58f14161c5ac9286f9ca94d541a8dd335155
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-14 18:01:55 +0100

    a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *main - Entry point
+ *Description: a program that prints the alphabet in lowercase
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	return (0);
+}
