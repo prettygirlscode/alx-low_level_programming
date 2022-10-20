6f02d8c97a2d0c23c165f1cf6d8d5f4962d8ce6f
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-20 23:19:14 +0100

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	for (n =1; n>=100; n++)
+
+		if (n % 3 = 0)
+			 printf("fizz");
+		else if (n%5 = 0)
+			 printf("buzz");
+
+		else if (n%3 = 0 && n%5 = 0)
+				 
+			printf("fizzbuzz");
+							    
+		else
+			printf("%d", n);
+
+	return 0;
+}
