482bf93c6090c21b52bcf42de96a5e4f4eef3206
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-28 00:10:08 +0100

    encodes string to i337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+
+
+/**
+ * *leet - encodes a string into 1337
+ * @s: string
+ * Return: s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char lower[] = "aeotl";
+	char upper[] = "AEOTL";
+	char n[] = "43071";
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (j = 0; lower[j] != 0; j++)
+		{
+			if (s[i] == lower[j] || s[i] == upper[j])
+			{
+				s[i] = n[j];
+
+			}
+
+		}
+	
+	}
+	return (s);
+}
