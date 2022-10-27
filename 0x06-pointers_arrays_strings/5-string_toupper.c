608a4c73d821ea08c907fc6261d6ecabf3f0b893
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-27 18:48:54 +0100

    change to uppercase string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - converts lower case to upper case
+ * @str: string to be capitalized
+ * Return: uppercase string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+
+}
