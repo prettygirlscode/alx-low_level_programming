0d9297dc8a04f60fcd340b2862b2b8e49a3bcde6
prettygirlscode <ransolinaotobia@icloud.com>
2022-10-31 19:09:03 +0100

    Wrote a function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
 
 /**
  * _strchr - a function that locates a character in a string.
- * @s: pointer to string to search
+ * @s: string to search
  * @c: character to find in s
  * Return: pointer to first occurrence of character c
  * or NULL if the character is not found
@@ -13,13 +13,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
 	}
+
 	return (NULL);
 }
-
