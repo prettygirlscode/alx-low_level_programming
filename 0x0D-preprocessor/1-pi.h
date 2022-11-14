e5b4c79a1b655621a1899cd87a0de43311c8baf7
prettygirlscode <ransolinaotobia@icloud.com>
2022-11-14 21:28:39 +0100

    a header file that defines a macro named PI as an abbreviation for the token 3.14159265359

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef _PIE_
+#define _PIE_
+
+/*
+ * File: 1-pi.h
+ * Auth: Patience Wellington
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
