ebd4d9945fad96c0096e7b5a28960e946cab9d8d
prettygirlscode <ransolinaotobia@icloud.com>
2022-11-14 21:25:22 +0100

    Header file that defines a macro named 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,8 +3,8 @@
 
 /*
  * File: 0-object_like_macro.h
- * Auth: Brennan D Baraban
- * Desc: Header file that defines a macro named
+ * Auth: Patience Wellington
+ * Desc: Header file defines a macro named
  *       SIZE as an abbreviation for the token 1024.
  */
 
