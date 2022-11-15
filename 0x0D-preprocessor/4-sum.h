5b311ac9d0659e66d10a6b9fda311dfa540ccaa0
prettygirlscode <ransolinaotobia@icloud.com>
2022-11-15 02:25:55 +0100

    a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef _FUNCTION_LIKE_MACRO_SUM_H
+#define _FUNCTION_LIKE_MACRO_SUM_H
+
+/*
+ * File: 4-sum.h
+ * Auth: Patience Wellington
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
