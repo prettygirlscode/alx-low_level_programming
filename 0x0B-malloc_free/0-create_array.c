77bc488501a704e59658de19cf59c947da3d85bf
prettygirlscode <ransolinaotobia@icloud.com>
2022-11-22 18:44:32 +0100

    add alternate solution

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -24,3 +24,24 @@ char *create_array(unsigned int size, char c)
 		p[i] = c;
 	return (p);
 }
+
+//alternate solution
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *str;
+
+	if (size == 0)
+		return NULL;
+	else if (str == 0)
+		return NULL;
+
+	str = malloc(sizeof(char) * size)
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+
+}
